c3f740934f54fc66f000bbc75e2673077fc85922
Marko Renic <markorenic@Markos-MacBook-Air.local>
2021-09-18 18:34:52 -0400

    minor changes

--- blockchain/contracts/ballot.sol
+++ blockchain/contracts/ballot.sol
@@ -3,5 +3,23 @@ pragma solidity >=0.4.22 <0.9.0;
 
 
 contract Ballot{
-	uint public candidateID = 0;  
+	uint public candidateCount = 0;  
+
+  struct Candidate{
+    uint id;
+    string name;
+    bool vote;
+  }
+
+  mapping(uint => Candidate) public candidates;
+  
+  constructor() public{
+    createCandidate("Alice");
+  }
+  
+  function createCandidate(string memory _content) public{
+  candidateCount ++;
+  candidates[candidateCount] = Candidate(candidateCount,_content, false);
+  }
+
 }
